bc8a78710c4ae7b1f0da4ea1891768c7c9a4e709
kingdaro <19603573+kingdaro@users.noreply.github.com>
2020-09-01 21:21:42 -0500

    StatInput: even simpler select on focus

--- src/components/StatInput.vue
+++ src/components/StatInput.vue
@@ -1,16 +1,15 @@
+<!-- TODO: add min/max -->
 <template>
 	<input
 		class="w-full px-3 py-2 text-center transition-colors duration-200 border-2 border-gray-300 rounded-md shadow-inner focus:outline-none focus:border-orange-500"
 		v-bind="$attrs"
 		:value="modelValue"
 		@input="handleInput"
-		@focus="selectAll($event.target)"
+		@focus="$event.target.select()"
 	/>
 </template>
 
 <script>
-import { selectAll } from "../helpers"
-
 export default {
 	props: {
 		label: String,
@@ -19,8 +18,6 @@ export default {
 
 	setup(props, context) {
 		return {
-			selectAll,
-
 			handleInput(event) {
 				context.emit("update:modelValue", Number(event.target.value) || 0)
 			},
