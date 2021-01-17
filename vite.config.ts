bce9de6a170a14e8fa0cb3e10f71413a1b7d25c6
MapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2021-01-17 14:13:19 -0600

    fancy plugin function names

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
-import preactRefresh from "@prefresh/vite"
+import prefresh from "@prefresh/vite"
 import { defineConfig } from "vite"
-import babelMacros from "vite-plugin-babel-macros"
+import macros from "vite-plugin-babel-macros"
 
 export default defineConfig({
-	plugins: [preactRefresh(), babelMacros()],
+	plugins: [prefresh(), macros()],
 	esbuild: {
 		jsxInject: `import { h, Fragment } from 'preact'`,
 		jsxFactory: "h",
