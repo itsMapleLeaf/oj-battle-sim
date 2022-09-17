e2d691b67d76a3f5423d545b4ebaac382e093f50
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2022-09-17 12:49:04 -0500

    upgrades

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,12 @@
-import prefresh from "@prefresh/vite"
+import preact from "@preact/preset-vite"
 import { defineConfig } from "vite"
 
 export default defineConfig({
-	plugins: [prefresh()],
-	alias: {
-		react: "preact/compat",
-		"react-dom": "preact/compat",
-	},
-	esbuild: {
-		jsxInject: `import { h, Fragment } from 'preact'`,
-		jsxFactory: "h",
-		jsxFragment: "Fragment",
-	},
-	optimizeDeps: {
-		include: ["twind/colors"],
+	plugins: [preact()],
+	resolve: {
+		alias: {
+			react: "preact/compat",
+			"react-dom": "preact/compat",
+		},
 	},
 })
