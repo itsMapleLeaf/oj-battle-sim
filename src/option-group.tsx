2589e589e191c098c2428ec26d028b0018a110e5
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2022-09-18 09:26:59 -0500

    share the border style for inputs

--- src/option-group.tsx
+++ src/option-group.tsx
@@ -1,5 +1,7 @@
 import type { Signal } from "@preact/signals"
+import classNames from "classnames"
 import { Fragment } from "preact/jsx-runtime"
+import { controlBorderClass } from "./styles"
 
 export function OptionGroup<T extends string>({
 	name,
@@ -11,7 +13,12 @@ export function OptionGroup<T extends string>({
 	valueSignal: Signal<T>
 }) {
 	return (
-		<div class="flex border-2 rounded-md w-fit border-stone-300 dark:border-stone-500 overflow-clip focus-within:border-orange-500 dark:focus-within:border-orange-500">
+		<div
+			class={classNames(
+				controlBorderClass,
+				"flex border-2 rounded-md w-fit overflow-clip",
+			)}
+		>
 			{options.map(({ label, value }) => (
 				<Fragment key={value}>
 					<label>
@@ -26,7 +33,7 @@ export function OptionGroup<T extends string>({
 								valueSignal.value = value
 							}}
 						/>
-						<div class="peer-checked:bg-orange-600 peer-checked:text-white leading-none p-3 hover:bg-orange-400/30 transition cursor-pointer dark:bg-black/20  dark:hover:bg-orange-400/20 dark:peer-checked:hover:bg-orange-600">
+						<div class="peer-checked:bg-orange-600 peer-checked:text-white leading-none p-3 hover:bg-orange-400/30 transition cursor-pointer dark:bg-black/20  dark:hover:bg-orange-400/20 dark:peer-checked:hover:bg-orange-600 select-none">
 							{label}
 						</div>
 					</label>
--- src/styles.ts
+++ src/styles.ts
@@ -1,2 +1,10 @@
-export const textInputClass =
-	"border-2 border-stone-300 dark:border-stone-500 shadow-inner rounded-md focus:bg-black-30 leading-none w-full p-3 dark:bg-black/20  focus:outline-none focus:border-orange-500 dark:focus:border-orange-500"
+import classNames from "classnames"
+
+export const controlBorderClass = classNames(
+	"border-2 border-stone-300 transition dark:border-stone-500 focus:border-orange-500 dark:focus:border-orange-500 focus-within:border-orange-500 dark:focus-within:border-orange-500",
+)
+
+export const textInputClass = classNames(
+	controlBorderClass,
+	"shadow-inner rounded-md focus:bg-black-30 leading-none w-full p-3 dark:bg-black/20 focus:outline-none",
+)
