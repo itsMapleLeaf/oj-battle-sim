5808557c139ea0f44896503656f2127e224b282d
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2022-09-19 02:32:59 -0500

    signals are cool sometimes but not always

--- src/theme.tsx
+++ src/theme.tsx
@@ -1,29 +1,26 @@
-import { effect, signal } from "@preact/signals"
 import { Moon, Sun } from "preact-feather"
+import { useEffect, useState } from "preact/hooks"
 import { SolidButton } from "./solid-button"
 
-const darkMode = signal(
-	typeof window !== "undefined"
-		? localStorage.getItem("darkMode") === "true"
-		: false,
-)
-if (typeof window !== "undefined") {
-	effect(() => {
-		localStorage.setItem("darkMode", darkMode.value ? "true" : "false")
-		document.documentElement.classList.toggle("dark", darkMode.value)
-	})
-}
-
 export function ThemeButton() {
+	const [darkMode, setDarkMode] = useState(() =>
+		typeof window !== "undefined"
+			? localStorage.getItem("darkMode") === "true"
+			: false,
+	)
+
+	useEffect(() => {
+		localStorage.setItem("darkMode", darkMode ? "true" : "false")
+		document.documentElement.classList.toggle("dark", darkMode)
+	}, [darkMode])
+
 	return (
 		<SolidButton
 			type="button"
 			title="Toggle theme"
-			onClick={() => {
-				darkMode.value = !darkMode.value
-			}}
+			onClick={() => setDarkMode(!darkMode)}
 		>
-			{darkMode.value ? <Moon /> : <Sun />}
+			{darkMode ? <Moon /> : <Sun />}
 		</SolidButton>
 	)
 }
